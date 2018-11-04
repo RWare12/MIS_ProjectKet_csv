b1a729e213621f77928a980c945cdc6ecbdef471
RWare12 <39044631+RWare12@users.noreply.github.com>
2018-11-04 23:07:19 +0800

    Add files via upload

---
+++ MIS_ProjectKet/MIS_ProjectKet/DataRecord.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIS_ProjectKet
+{
+    class DataRecord
+    {
+        public String CreateDate { get; set; }
+        public String IRInitialResponse { get; set; }
+        public String FRFixResponse { get; set; }
+        public String Summary { get; set; }
+        public String TicketNo { get; set; }
+        public String Company { get; set; }
+        public String Assignedto { get; set; }
+        public String Caller { get; set; }
+        public String CaseType { get; set; }
+        public String CloseDate { get; set; }
+        public String DaysOpen { get; set; }
+        public String Department { get; set; }
+        public String InfrastructureType { get; set; }
+        public String Location { get; set; }
+        public String Priority { get; set; }
+        public String IncidentType { get; set; }
+        public String Resolution { get; set; }
+        public String Shift { get; set; }
+
+
+    }
+}
---
+++ MIS_ProjectKet/MIS_ProjectKet/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+
+
+namespace MIS_ProjectKet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var sr = new StreamReader(@"C:\Users\suare\Desktop\Project\test.csv"))
+            {
+                var reader = new CsvReader(sr);
+
+                //CSVReader will now read the whole file into an enumerable
+                IEnumerable<DataRecord> records = reader.GetRecords<DataRecord>();
+
+                //CSV file will be printed to the Output Window
+                int row = 0;
+                foreach (DataRecord record in records)
+                {
+                    Console.WriteLine("row: " + row++);
+                    Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}, {12}" +
+                        ", {13}, {14}, {15}, {16}, {17}", record.CreateDate, record.IRInitialResponse, record.FRFixResponse,
+                        record.Summary, record.TicketNo, record.Company, record.Assignedto, record.Caller, record.CaseType,
+                        record.CloseDate, record.DaysOpen, record.Department, record.InfrastructureType, record.Location,
+                        record.Priority, record.IncidentType, record.Resolution, record.Shift);
+                    
+                }
+            }
+
+
+            Console.WriteLine("FINISHED");
+            Console.ReadKey();
+        }
+
+    }
+}
